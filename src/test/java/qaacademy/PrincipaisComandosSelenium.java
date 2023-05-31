986e6b10ab109d5ad6e11d6f78b51354aad0c39a
ViniciusBarros90 <vinicius_santoss@hotmail.com>
2023-05-31 18:30:28 -0300

    Teste com Send Keys

--- src/test/java/qaacademy/PrincipaisComandosSelenium.java
+++ src/test/java/qaacademy/PrincipaisComandosSelenium.java
@@ -5,18 +5,22 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-
 public class PrincipaisComandosSelenium {
-   
+
     @Test
-   public void testeSeleniumClick () throws InterruptedException{ //metodos
-    
-    WebDriver driver = new ChromeDriver(); //Abrir o Browser
-    driver.get("https://www.qaacademy.com.br/");
-    Thread.sleep(3000);
-    driver.findElement(By.xpath("/html/body/div[2]/div/div/div[1]/button[1]")).click();
-   
-    
+    public void testeSeleniumEnviarTextosParaTela() throws InterruptedException { // metodos
+
+        WebDriver driver = new ChromeDriver(); // Abrir o Browser
+        driver.get("https://www.qaacademy.com.br/");
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("/html/body/div[2]/div/div/div[1]/button[1]")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//*[@placeholder='Nome']")).click(); // Seleciona campo nome
+        driver.findElement(By.xpath("//*[@placeholder='Nome']")).sendKeys("Vinicius Barros");
+        driver.findElement(By.xpath("//*[@placeholder='Email']")).click(); // Seleciona campo email
+        driver.findElement(By.xpath("//*[@placeholder='Email']")).sendKeys("teste@teste.com.br");
+        driver.findElement(By.xpath("//*[@id='comp-k37gdiro']/button/span")).click(); // Clicar no botão Enviar
+
     }
-    
+
 }
