1210371d726cdf71a26eb0357f634348af401101
ViniciusBarros90 <vinicius_santoss@hotmail.com>
2023-05-31 18:09:07 -0300

    Teste Selenium click alteração

--- src/test/java/qaacademy/PrincipaisComandosSelenium.java
+++ src/test/java/qaacademy/PrincipaisComandosSelenium.java
@@ -4,8 +4,7 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+
 
 public class PrincipaisComandosSelenium {
    
