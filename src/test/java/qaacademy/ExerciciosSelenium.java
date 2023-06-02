ef869bc40545a2ee6f8da3875667e8140c94cce6
ViniciusBarros90 <vinicius_santoss@hotmail.com>
2023-06-02 12:51:02 -0300

    Selecionando um ou mais item da lista e validando que os mesmos foram selecionados e exibido

--- src/test/java/qaacademy/ExerciciosSelenium.java
+++ src/test/java/qaacademy/ExerciciosSelenium.java
@@ -35,27 +35,34 @@ public class ExerciciosSelenium {
         driver.findElement(By.xpath("//*[@ng-model='radiovalue' and @value='Male']")).click();
         // Validando que o radio buttom "Gender" foi selecionado
         Assert.assertTrue("Elemento não selecionado",
-        driver.findElement(By.xpath("//*[@ng-model='radiovalue' and @value='Male']")).isSelected());
+                driver.findElement(By.xpath("//*[@ng-model='radiovalue' and @value='Male']")).isSelected());
 
-        // Selecionando o Checkbox "Movies" 
+        // Selecionando o Checkbox "Movies"
         driver.findElement(By.xpath("//input[@value='Movies']")).click();
         // Validando que o radio o Checkbox "Movies" foi selecionado
         Assert.assertTrue("Checkbox não selecionado",
-        driver.findElement(By.xpath("//input[@value='Movies']")).isSelected());
+                driver.findElement(By.xpath("//input[@value='Movies']")).isSelected());
 
         driver.findElement(By.xpath("//*[@id='msdd']")).click();
         Thread.sleep(1000);
 
-        //Selecionar lista
+        // Selecionar lista
         driver.findElement(By.xpath("//*[@id='msdd']")).click();
         driver.findElement(By.xpath("//a[contains(text(), 'Portuguese')]")).click();
         driver.findElement(By.xpath("//a[contains(text(), 'English')]")).click();
-        
 
-boolean LinguagemPortuguesSelecionada = driver
-        //Validando que Portugues e English foram selecionados e exibidos
-        Assert.assertTrue(driver.findElement(By.xpath("//div[contains(text(),'Portuguese') and @class='ui-autocomplete-multiselect-item']")).isDisplayed());
-        Assert.assertTrue(driver.findElement(By.xpath("//div[contains(text(),'English') and @class='ui-autocomplete-multiselect-item']")).isDisplayed());
+        // Validando que Portugues e English foram selecionados e exibidos
+        boolean LinguagemPortuguesSelecionada = driver
+                .findElement(
+                        By.xpath("//div[contains(text(),'Portuguese') and @class='ui-autocomplete-multiselect-item']"))
+                .isDisplayed();
+
+        boolean LinguagemEnglishSelecionada = driver
+                .findElement(
+                        By.xpath("//div[contains(text(),'English') and @class='ui-autocomplete-multiselect-item']"))
+                .isDisplayed();
+        Assert.assertTrue(LinguagemPortuguesSelecionada && LinguagemEnglishSelecionada);
+
     }
 
     @AfterClass
